e05333b5bb383b3b27cb7c14366571b0e8841f77
lautaropastorino <lautaro.pastorino@hotmail.com>
2020-09-17 21:00:51 -0300

    Low energy ? strategy

--- RobocodeLabo/src/laboratorio/LowEnergyStrategy.java
+++ RobocodeLabo/src/laboratorio/LowEnergyStrategy.java
@@ -5,41 +5,50 @@ import java.util.concurrent.ThreadLocalRandom;
 public class LowEnergyStrategy implements RobotStrategy {
 	
 	private Karl karl;
+	private int move;
 	
 	public LowEnergyStrategy(Karl karl) {
 		this.karl = karl;
+		move = 1;
 	}
 
 	@Override
 	public void onScannedRobot() {
-		int generatedAngle = ThreadLocalRandom.current().nextInt(karl.scannedAngle - 2, karl.scannedAngle + 3);
-		karl.turnGunTo(generatedAngle);
-		karl.fire(1);	
-		
+		this.karl.turnGunTo(this.karl.scannedAngle);
+		this.karl.fire(3);
+		if (this.karl.scannedDistance <= 200) {
+			if (this.karl.scannedDistance <= 100) {
+				System.out.println("Cerca");
+				move = 2;
+			} else {
+				move = 3;
+			}
+		}
 	}
 
 	@Override
 	public RobotStrategy onHitByBullet() {
+		this.karl.turnRight(90 - this.karl.hitByBulletBearing);
 		return this;
-		
 	}
 
 	@Override
 	public void onHitWall() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		int exceso = ThreadLocalRandom.current().nextInt(-20, 21);
+		this.karl.ahead((100 + exceso) * move);
+		System.out.println("Move = " + move);
+		this.karl.turnGunRight(360);
+		this.karl.back((100 + exceso) * move);
+		move = 1;
 	}
 
 	@Override
 	public void onHitRobot() {
-		// TODO Auto-generated method stub
-		
+		//move = 0;
 	}
 
 }
