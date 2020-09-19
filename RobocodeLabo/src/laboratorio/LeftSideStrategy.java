b12b088e8337a95d29cabdc3386cf06fdd05a1cc
lautaropastorino <lautaro.pastorino@hotmail.com>
2020-09-19 20:14:13 -0300

    Arreglo los comentarios para hacer mas legible el codigo

--- RobocodeLabo/src/laboratorio/LeftSideStrategy.java
+++ RobocodeLabo/src/laboratorio/LeftSideStrategy.java
@@ -13,9 +13,7 @@ public class LeftSideStrategy implements RobotStrategy{
 
 	@Override
 	public void onScannedRobot() {
-		// Le apunto al más cercano
 		this.karl.turnGunTo(this.karl.scannedAngle);
-		// Disparamos a toda potencoa
 		this.karl.fire(3);
 		// Si está muy cerca el escaneado
 		if (this.karl.scannedDistance <= 50) {
@@ -34,25 +32,18 @@ public class LeftSideStrategy implements RobotStrategy{
 
 	@Override
 	public void run() {
-		// Si ya estamos a la izquierda
+		/* Si ya estamos a la izquierda nos ponemos mirando hacia arriba y subimos
+		 * y bajamos escaneado solo hacia la derecha */
 		if (this.karl.robotX <= 100) {
-			// Generamos un modificador aleatorio de distancia
 			int exceso = ThreadLocalRandom.current().nextInt(-20, 21);
-			// Enderezamos la trompa hacia arriba
 			this.karl.turnTo(0);
-			// Ponemos el arma hacia arriba
 			this.karl.turnGunTo(0);
-			// Avanzamos con el modificador aleatorio y con el modificador de distancia
 			this.karl.ahead((100 + exceso) * move);
-			// Escaneamos en sentido de las agujas del reloj los 180 grados
 			this.karl.turnGunRight(180);
-			// Retrocedemos 
 			this.karl.back((100 + exceso) * move);
-			// Escaneamos en sentido contrario a las agujas del reloj 180 grados
 			this.karl.turnGunLeft(180);
-		// Si no estamos a la izquierda
+		/* Si no estamos a la izquierda, apuntamos hacia allá y vamos. Tambien escaneamos */
 		} else {
-			// Ponemos la trompa hacia la izquierda
 			this.karl.turnTo(270);
 			this.karl.ahead(100);
 			this.karl.turnGunRight(180);
@@ -65,10 +56,8 @@ public class LeftSideStrategy implements RobotStrategy{
 	public void onHitRobot() {
 		// Si chocamos de frente
 		if (this.karl.hitRobotBearing < 90) {
-			// Vamos hacia atrás
 			this.karl.turnBackLeft(100, 90);
 		} else {
-			// Vamos hacia adelante
 			this.karl.turnAheadRight(100, 90);
 		}
 	}
